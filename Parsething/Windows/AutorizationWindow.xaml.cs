f33d3ea9dd742572d38507123a14222119ecc81d
Дениска Зеленцов <zelentsov36@gmail.com>
2023-03-29 14:15:53 +0300

    v0.0.0.1

--- Parsething/Windows/AutorizationWindow.xaml.cs
+++ Parsething/Windows/AutorizationWindow.xaml.cs
@@ -24,11 +24,16 @@ public partial class AutorizationWindow : Window
 
     private void PasswordVisibility_Checked(object sender, RoutedEventArgs e)
     {
-
+        Password.Visibility = Visibility.Hidden;
+        VisiblePassword.Visibility = Visibility.Visible;
+        VisiblePassword.Text = Password.Password;
+        
     }
 
     private void PasswordVisibility_Unchecked(object sender, RoutedEventArgs e)
     {
-
+        Password.Visibility = Visibility.Visible;
+        VisiblePassword.Visibility = Visibility.Hidden;
+        Password.Password = VisiblePassword.Text;
     }
 }
