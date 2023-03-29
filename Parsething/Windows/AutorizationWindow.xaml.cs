e7d26d40bd382fc3f72843ee98dfab4296824b8f
c1ose-d <michael.glodin2004@gmail.com>
2023-03-29 12:54:08 +0300

    v0.0.0.1

---
+++ Parsething/Windows/AutorizationWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Parsething.Windows;
+
+public partial class AutorizationWindow : Window
+{
+    public AutorizationWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
+    private void MinimizeAction_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void CloseAction_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void PasswordVisibility_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void PasswordVisibility_Unchecked(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
